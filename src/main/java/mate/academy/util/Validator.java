2a5cfbd8beff654051689f2c0bb66bc82ec780e0
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-09 17:49:48 +0300

    fix: proper exceptions

--- src/main/java/mate/academy/util/Validator.java
+++ src/main/java/mate/academy/util/Validator.java
@@ -5,12 +5,6 @@ import java.util.regex.Pattern;
 public class Validator {
     private static final String EMAIL_REGEXP = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
             + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-
-    /* Password must contain at least one digit [0-9].
-    Password must contain at least one lowercase Latin character [a-z].
-    Password must contain at least one uppercase Latin character [A-Z].
-    Password must contain at least one special character like ! @ # & ( ).
-    Password must contain a length of at least 8 characters and a maximum of 20 characters. */
     private static final String PASSWORD_REGEXP
             = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
 
